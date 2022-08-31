bb01c1bad6c90b8f331d9fbe7554be0cf2f5874e
valterjoaosilva <valterjoaosilva@gmail.com>
2022-08-31 19:51:31 -0300

    estrutura de repetição

---
+++ condicionais.py
@@ -0,0 +1,11 @@
+n1 = int(input("informe um valor inteiro: "))
+n2 = int(input("informe um valor inteiro: "))
+
+
+if(n1 == n2):
+    print('Os valores são iguais: '+ str(n1)+ ' e ' +str(n2))
+elif(n1 > n2):
+    print('n1 é maior do que n2.')
+else:
+    print("n2 é maior do que n1")
+    
--- operadorMatematico.py
+++ operadorMatematico.py
@@ -1,3 +1,13 @@
-n1 = int(input("informe um valor inteiro: "))
-n2 = int(input("informe um valor inteiro: "))
+n1 = float(input("informe um valor inteiro: "))
+n2 = float(input("informe um valor inteiro: "))
 print("resultado da adição: n1 + n2 = " + str(n1 + n2) )
+print("resultado da subtração: n1 - n2 = " + str(n1 - n2) )
+print("resultado da multiplicação: n1 x n2 = " + str(n1 * n2) )
+#controlando casas decimais
+#passo percentual ponto número de casas que desejo flutuar
+#como vou definir através do simbolo percentual quais variáveis vão receber a variação de ponto flutuante não precisa concatenar e nem mesmo converter
+print("resultado da adição: n1 / n2 = %.2f" % (n1 / n2) )
+
+#convertendo em mais de duas casas decimais e trabalhando com ponto flutuante
+print("resultado da adição primeiro valor %.3f, segundo valor %.5f: " % (n1,  n2))
+print("resultado da adição: n1 + n2 = %.2f: " % (n1 + n2) )
