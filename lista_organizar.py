76c2ddf7036154f903505419e94ab2a20e27894f
valterjoaosilva <valterjoaosilva@gmail.com>
2022-09-03 20:22:04 -0300

    tuplas

--- lista_organizar.py
+++ lista_organizar.py
@@ -1,1 +1,8 @@
 carros = ["Fusca", "Gol", "Corsel", "Galaxy", "Doged", "Corsa"]
+#ordenando listas
+carros.sort()
+print(carros)
+#invertando listas
+carros.reverse()
+print(carros)
+print(len(carros))
---
+++ tuplas.py
@@ -0,0 +1,5 @@
+#tuplas
+carro = ('Corcel', 'chevete', 'passat')
+for x in range(0,len(carro)):
+    print (x, carro[x])
+    
